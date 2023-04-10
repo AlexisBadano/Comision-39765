919d2eac90542fd7ebdc29e525e7a5c978ce8239
Alexis Badano <Alexis.badano@gmail.com>
2023-04-10 11:18:00 -0300

    subiendo carpeta

--- Desafio 2/index.js
+++ Desafio 2/index.js
@@ -40,7 +40,7 @@ const context = async () => {
 
     // await productManager.addProduct(testProduct2)
     // await productManager.addProduct(testProduct3)
-    // await productManager.addProduct(testProduct4)
+    await productManager.addProduct(testProduct4)
     await productManager.updateProduct(3,testProduct);
     // await productManager.deleteProduct(1);
 }
