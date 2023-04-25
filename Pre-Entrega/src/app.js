91a7c1883c8c59bd5e3bee14e590f82009cb3ae3
Alexis Badano <Alexis.badano@gmail.com>
2023-04-24 22:14:36 -0300

    Primer Pre-Entrega

---
+++ Pre-Entrega/src/app.js
@@ -0,0 +1,63 @@
+import ProductManager from "./managers/productManager.js";
+import express from "express";
+import productsRouter from "./routes/products.router.js";
+import cartsRouter from "./routes/carts.router.js";
+
+const productManager = new ProductManager();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use('/products', productsRouter)
+app.use("/carts", cartsRouter);
+
+const context = async () => {
+    
+
+    app.listen(8080, () => {
+        "Listening on 8080";
+    });
+
+    const testProduct = {
+        title: "PC Gamer",
+        description: "descripcion aqui",
+        price: 500000,
+        thumbnail: "no tiene",
+        code: 1,
+        stock: 50,
+    };
+    const testProduct2 = {
+        title: "PC Office",
+        description: "descripcion aqui4",
+        price: 150500,
+        thumbnail: "no tiene",
+        code: 2,
+        stock: 25,
+    };
+    const testProduct3 = {
+        title: "Notebook",
+        description: "descripcion aqui4",
+        price: 250500,
+        thumbnail: "no tiene",
+        code: 3,
+        stock: 50,
+    };
+    const testProduct4 = {
+        title: "Monitor",
+        description: "descripcion aqui4",
+        price: 45000,
+        thumbnail: "no tiene",
+        code: 4,
+        stock: 10,
+    };
+
+    // await productManager.addProduct(testProduct2)
+    // await productManager.addProduct(testProduct3)
+    // await productManager.addProduct(testProduct4)
+    // await productManager.updateProduct(3,testProduct);
+    // await productManager.deleteProduct(3);
+    // await productManager.getProducts();
+};
+
+context();
---
+++ Pre-Entrega/src/managers/cartManager.js
@@ -0,0 +1,108 @@
+import fs from 'fs'
+
+import ProductManager from './productManager.js'
+
+const productManager = new ProductManager()
+// const products = productManager.getProducts();
+
+export default class CartManager {
+    constructor() {
+        (this.carts = []), (this.path = "../files/carts.json");
+    }
+
+    appendCarts = async () => {
+        try {
+            await fs.promises.writeFile(
+                this.path,
+                JSON.stringify(this.carts, null, "\t")
+            );
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    getCarts = async () => {
+        try {
+            if (fs.existsSync(this.path)) {
+                const data = await fs.promises.readFile(this.path, "utf-8");
+                const carts = JSON.parse(data);
+                return carts;
+            }
+            return [];
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    getCartById = async (Id) => {
+        try {
+            const cart = await this.getCarts();
+            const cartId = cart.find((cart) => cart.id === Id);
+            if (!cartId) {
+                return console.log("Cart Not found");
+            } else {
+                return cartId;
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    createCarts = async (cart) => {
+        try {
+            const carts = await this.getCarts();
+
+            if (carts.length === 0) {
+                cart.id = 1;
+            } else {
+                cart.id = carts[carts.length - 1].id + 1
+
+            }
+            if (typeof cart.products === "undefined") {
+                cart.products = [];
+            }
+            carts.push(cart);
+
+            await fs.promises.writeFile(this.path, JSON.stringify(carts, null, "\t"));
+            console.log("Se ha agregado el nuevo carrito!")
+            
+            return cart;
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    addProduct = async (cid, pid, quantity) => {
+
+        const carts = await this.getCarts();
+        const cart = carts.find((c) => c.id === cid) || {};
+
+
+        const product = await productManager.getProductById(pid);
+        if (Object.keys(cart).length === 0) {
+            return "No cart found";
+        }
+        if (typeof product !== "object") {
+            return "No product found";
+        }
+        if (quantity === 0) {
+            return "Quantity must be higher than 0";
+        }
+        const prodInCart = cart['products'].find((prod) => prod.id === pid) || {};
+
+        if (Object.keys(prodInCart).length === 0) {
+            prodInCart.id = pid;
+            prodInCart.quantity = quantity;
+
+            cart.products.push(prodInCart);
+
+        } else {
+            prodInCart.quantity = prodInCart.quantity + quantity;
+
+        }
+        this.carts = carts;
+        this.appendCarts();
+        return prodInCart;
+
+    };
+}
